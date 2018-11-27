c50d12ad0f8c65795bc5a922c2336af94a9836be
Tim Regan <timregan@microsoft.com>
2018-11-27 17:04:37 +0000

    Done server, but untested

--- WebSocketServer/ContainerApp/MainPage.xaml.cs
+++ WebSocketServer/ContainerApp/MainPage.xaml.cs
@@ -1,38 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using WebSocketServerStandard;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ContainerApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private WebSocketServer server;
         public MainPage()
         {
             this.InitializeComponent();
-            server = new WebSocketServer()
         }
 
         private void StartServerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (server == null)
+            {
+                server = new WebSocketServer(8181, "http://localhost:8080", "ws://localhost:8181/");
+                server.Logger = new DebugTextWriter(); //Console.Out;
+                server.LogLevel = ServerLogLevel.Subtle;
+                server.ClientConnected += Server_ClientConnected;
+                server.Start();
+                StartServerButton.IsEnabled = false;
+            }
         }
+
+        private void Server_ClientConnected(WebSocketConnection sender, EventArgs e)
+        {
+            CommunicationTextBlock.Text = "Connected";
+            sender.Disconnected += Sender_Disconnected;
+            sender.DataReceived += Sender_DataReceived;
+        }
+
+        private void Sender_DataReceived(WebSocketConnection sender, WebSocketServerStandard.DataReceivedEventArgs e)
+        {
+            CommunicationTextBlock.Text = e.Data;
+            Debug.WriteLine($"From client: {e.Data}");
+        }
+
+        private void Sender_Disconnected(WebSocketConnection sender, EventArgs e)
+        {
+            CommunicationTextBlock.Text = "Disconnected";
+            server = null;
+            StartServerButton.IsEnabled = false;
+        }
+    }
+
+    // https://stackoverflow.com/a/4234085/575530
+    public class DebugTextWriter : StreamWriter
+    {
+        public DebugTextWriter() : base(new DebugOutStream(), Encoding.Unicode, 1024)
+        {
+            this.AutoFlush = true;
+        }
+
+        public override Encoding Encoding => throw new NotImplementedException();
+
+        class DebugOutStream : Stream
+        {
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                Debug.Write(Encoding.Unicode.GetString(buffer, offset, count));
+            }
+
+            public override bool CanRead { get { return false; } }
+            public override bool CanSeek { get { return false; } }
+            public override bool CanWrite { get { return true; } }
+            public override void Flush() { Debug.Flush(); }
+            public override long Length { get { throw new InvalidOperationException(); } }
+            public override int Read(byte[] buffer, int offset, int count) { throw new InvalidOperationException(); }
+            public override long Seek(long offset, SeekOrigin origin) { throw new InvalidOperationException(); }
+            public override void SetLength(long value) { throw new InvalidOperationException(); }
+            public override long Position
+            {
+                get { throw new InvalidOperationException(); }
+                set { throw new InvalidOperationException(); }
+            }
+        };
     }
 }
