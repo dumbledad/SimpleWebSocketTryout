75989eb4e8c3338b5fa2b3661a8c65fdf6cccc64
Tim Regan <timregan@microsoft.com>
2018-12-05 17:25:43 +0000

    WiP parsing out frames

---
+++ WebSocketServer/SimpleWebSocketServer/WebSocketClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleWebSocketServer
+{
+    public delegate void DataReceivedEventHandler(WebSocketConnection sender, EventArgs e);
+    public delegate void WebSocketDisconnectedEventHandler(WebSocketConnection sender, EventArgs e);
+
+    public class WebSocketConnection
+    {
+        // https://tools.ietf.org/html/rfc6455
+        private const string _ExtensionsPrefix = "Sec-WebSocket-Extensions:";
+        private const string _ProtocolPrefix = "Sec-WebSocket-Protocol:";
+        private const string _VersionPrefix = "Sec-WebSocket-Version:";
+        private const string _KeyPrefix = "Sec-WebSocket-Key:";
+        private const string _GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+        public event DataReceivedEventHandler DataReceivedEvent;
+        public event WebSocketDisconnectedEventHandler WebSocketDisconnected;
+
+        private string hashedKey = "";
+        private List<byte> bytesReceived = new List<byte>();
+
+        public List<string> RequestedExtensions { get; private set; } = new List<string>();
+        public List<string> RequestedProtocols { get; private set; } = new List<string>();
+        public string Version { get; private set; }
+        public string Key { get; private set; }
+        public Socket Socket { get; private set; }
+        public string HashedKey
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(hashedKey))
+                {
+                    using (var sha1 = new SHA1Managed())
+                    {
+                        hashedKey = Convert.ToBase64String(sha1.ComputeHash(Encoding.UTF8.GetBytes(Key + _GUID)));
+                    }
+                }
+                return hashedKey;
+            }
+        }
+        public int BufferSize { get; set; } = 511;
+
+        public WebSocketConnection(Socket socket, IEnumerable<string> clientHandshakeLines)
+        {
+            Socket = socket;
+
+            var requestedExtensionsLine = clientHandshakeLines.FirstOrDefault(line => line.StartsWith(_ExtensionsPrefix));
+            RequestedExtensions = requestedExtensionsLine.Substring(_ExtensionsPrefix.Length).Split(';').Select(fragment => fragment.Trim()).ToList();
+
+            var requestedProtocolsLine = clientHandshakeLines.FirstOrDefault(line => line.StartsWith(_ProtocolPrefix));
+            RequestedExtensions = requestedProtocolsLine.Substring(_ProtocolPrefix.Length).Split(';').Select(fragment => fragment.Trim()).ToList();
+
+            var versionLine = clientHandshakeLines.FirstOrDefault(line => line.StartsWith(_VersionPrefix));
+            Version = versionLine.Substring(_VersionPrefix.Length).Trim();
+
+            var keyLine = clientHandshakeLines.FirstOrDefault(line => line.StartsWith(_KeyPrefix));
+            Key = versionLine.Substring(_KeyPrefix.Length).Trim();
+        }
+
+        public void BeginReceive()
+        {
+            Socket.BeginReceive(new byte[BufferSize], 0, BufferSize, 0, OnRead, null);
+        }
+
+        private void OnRead(IAsyncResult asyncResult)
+        {
+            var sizeOfReceivedData = Socket.EndReceive(asyncResult);
+            if (sizeOfReceivedData > 0)
+            {
+                //TODO: get bytes and parse out frame
+            }
+            else // the socket is closed
+            {
+                WebSocketDisconnected?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private bool TryParseBytes(IEnumerable<byte> bytes, out WebSocketFrame result)
+        {
+            WebSocketFrame
+        }
+    }
+}
---
+++ WebSocketServer/SimpleWebSocketServer/WebSocketServer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleWebSocketServer
+{
+    public delegate void ClientConnectedEventHandler(WebSocketServer sender, EventArgs e);
+
+    public class WebSocketServer
+    {
+        public event ClientConnectedEventHandler ClientConnected;
+
+        private List<WebSocketConnection> clients = new List<WebSocketConnection>();
+
+        public int Port { get; private set; }
+        public Socket ListenerSocker { get; private set; }
+        public IEnumerable<WebSocketConnection> Clients
+        {
+            get
+            {
+                return new ReadOnlyCollection<WebSocketConnection>(clients);
+            }
+        }
+        public int BufferSize { get; set; } = 511;
+
+        public WebSocketServer(int port) => Port = port;
+
+        public void Start()
+        {
+            ListenerSocker = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            ListenerSocker.Bind(new IPEndPoint(IPAddress.Any, Port));
+            ListenerSocker.Listen(100);
+            Debug.WriteLine($"websocket server stated on {ListenerSocker.LocalEndPoint}");
+            ListenerSocker.BeginAccept(new AsyncCallback(OnClientConnect), null);
+        }
+
+        private void OnClientConnect(IAsyncResult asyn)
+        {
+            var clientSocket = ListenerSocker.EndAccept(asyn);
+
+            var lines = new List<string>();
+
+            using (var stream = new NetworkStream(clientSocket))
+            using (var reader = new StreamReader(stream))
+            {
+                string line = reader.ReadLine();
+                while (line != "")
+                {
+                    lines.Add(line);
+                    Debug.WriteLine($"websocket client sent {line}");
+                    line = reader.ReadLine();
+                }
+            }
+
+            var client = new WebSocketConnection(clientSocket, lines);
+            clients.Add(client);
+            SendOpeningHandShake(client);
+            ClientConnected?.Invoke(this, EventArgs.Empty);
+            client.BeginReceive();
+        }
+
+        private void SendOpeningHandShake(WebSocketConnection client)
+        {
+            using (var stream = new NetworkStream(client.Socket))
+            using (var writer = new StreamWriter(stream))
+            {
+                //TODO: When we send a handshake to the client we are not acknowledging and thus agreeing to any of the requested extensions or protocols. These could be implemented and agreed to, e.g. compression.
+                writer.WriteLine("HTTP/1.1 101 Web Socket Protocol Handshake");
+                writer.WriteLine("Upgrade: WebSocket");
+                writer.WriteLine("Connection: Upgrade");
+                writer.WriteLine($"Sec-WebSocket-Accept: {client.HashedKey}");
+                writer.WriteLine("");
+            }
+        }
+    }
+}
